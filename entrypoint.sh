1a707f994bf130a3bdffe5d7d19c7dce5aa4b168
Temirkhan <temirkhan@nasukhov.me>
2024-03-30 16:22:46 +0100

    Adjusted deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /app
 # TODO research more adequate ways to do this
-./gradlew clean jar
-./gradlew --stop
-
+./gradlew clean jar && \
+./gradlew update && \
+./gradlew --stop && \
 java -jar build/libs/TukitaLearner-1.0.jar
