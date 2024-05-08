9105f6c456e3dc7ca3d8af0a4045cb58901c849c
Temirkhan <temirkhan@nasukhov.me>
2024-05-08 11:34:08 +0200

    Fixed migration

--- src/main/resources/migrations/20240501162600.sql
+++ src/main/resources/migrations/20240501162600.sql
@@ -3,6 +3,6 @@ ALTER TABLE learned_words RENAME COLUMN word_id TO resource_id;
 ALTER TABLE learned_words RENAME COLUMN channel_id TO group_id;
 ALTER TABLE learned_words RENAME TO learned_resources;
 
-CREATE INDEX channel_resource_idx ON learned_resources (channel_id, resource_type);
+CREATE INDEX channel_resource_idx ON learned_resources (group_id, resource_type);
 
 DROP INDEX idx_channel_id;
